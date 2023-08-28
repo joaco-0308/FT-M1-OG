67528228bcd0bd60ef1ed25485f78029092a4224
J JoaquínCastillo <117773656+joaco-0308@users.noreply.github.com>
2023-08-28 14:58:07 -0300

    n

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,26 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  let contador = 0; 
+  let array = num.split("").reverse(); 
 
-function DecimalABinario(num) {}
+  for (let i = 0; i < array.length; i++) { 
+    contador += array[i] * 2 ** i; 
+  }
+  return contador; 
+}
+
+function DecimalABinario(num) {
+let contador = []; 
+
+while(num > 0){ 
+   contador = num % 2 + contador; 
+   num = Math.floor(num / 2); 
+}
+return contador; 
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
